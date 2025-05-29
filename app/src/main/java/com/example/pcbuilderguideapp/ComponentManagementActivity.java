a4b6e410b92e0e78fc7b13a888a92570eb3a53bf
Ruko <dotruongthinh2212@gmail.com>
2025-05-29 22:14:59 +0700

    component management

---
+++ app/src/main/java/com/example/pcbuilderguideapp/ComponentManagementActivity.java
@@ -0,0 +1,10 @@
+package com.example.pcbuilderguideapp;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ComponentManagementActivity {
+}
