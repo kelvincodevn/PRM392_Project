420c9f85d3439723e5fcbfab23501b1514d3d2e9
Thinh <dotruongthinh2212@gmail.com>
2025-05-31 15:13:55 +0700

    add more tabs

--- app/src/main/java/com/example/pcbuilderguideapp/AdminDashboardActivity.java
+++ app/src/main/java/com/example/pcbuilderguideapp/AdminDashboardActivity.java
@@ -5,14 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
-import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminDashboardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin_dashboard);
+        setContentView(R.layout.admin_general_dashboard);
 
         View dropdownButton = findViewById(R.id.dropdown_container);
         dropdownButton.setOnClickListener(v -> {
